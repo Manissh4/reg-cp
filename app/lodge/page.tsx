ac730a41baa1048371b269198b47d4e78ffe68d6
Pranav Krishnaa S <pranavkrishnaa.s@goml.io>
2025-09-12 12:08:45 +0530

    created table and detailed info modal component

--- app/lodge/page.tsx
+++ app/lodge/page.tsx
@@ -1,55 +1,8 @@
 'use client'
 
-import SuccessModal from "@/components/SucessModal"
-import { CustomInput } from "@/components/ui/custom-input"
-import { useEffect, useState } from "react"
-import { FaCheckCircle } from "react-icons/fa";
-
 const Page = () => {
-  const [files, setFiles] = useState<File[]>([])
-  const [isModalOpen, setIsModalOpen] = useState(false)
-
-  useEffect(() => {
-    if (files.length > 0)
-      setIsModalOpen(true)
-  }, [files])
-
   return (
-    <div className="w-full flex flex-col items-center">
-      <CustomInput
-        type="upload"
-        containerClassName="w-full"
-        accept=".jpg, .jpeg, .png, .pdf"
-        multiple={true}
-        onFileInputChange={(e) => {
-          if (e.target.files) {
-            const selectedFiles = Array.from(e.target.files)
-            setFiles(selectedFiles)
-            console.log(selectedFiles)
-          }
-        }}
-      />
-      {files.length > 0 && (
-        <div className="mt-4">
-          <h3 className="text-lg font-medium mb-2">Selected Files:</h3>
-          {files.map((file, index) => (
-            <div key={index} className="text-sm text-gray-700">
-              {file.name} - {(file.size / 1024).toFixed(2)} KB
-            </div>
-          ))}
-        </div>
-      )}
-      {isModalOpen &&
-        <SuccessModal
-          title="Files Upload"
-          message="Files have been successfully uploaded."
-          handleModalClose={() => setIsModalOpen(false)}
-          titleIcon={<FaCheckCircle className="text-[#3C9718]" size={24} />}
-          ActionButtonText="Done"
-          handleAction={() => setIsModalOpen(false)}
-        />
-      }
-    </div>
+    <div>Lodge Grievance</div>
   )
 }
 
---
+++ components/DetailedInfoModal.tsx
@@ -0,0 +1,77 @@
+import { IoCloseOutline } from "react-icons/io5";
+import { CustomButton } from "./ui/CustomButton";
+import { cn } from "@/lib/utils";
+
+interface DetailedInfoModalProps {
+    title: string;
+    cancelButton?: boolean;
+    actionButtonText: string;
+    cancelButtonText?: string;
+    handleCancel?: () => void;
+    handleAction?: () => void;
+    handleModalClose: () => void;
+    leftButtonIcon?: React.ReactNode;
+    rightButtonIcon?: React.ReactNode;
+    children: React.ReactNode;
+    maxHeight?: string;
+    maxWidth?: string;
+    footer?: boolean;
+    footerButtonAlignment?: "start" | "end"
+}
+
+const DetailedInfoModal = ({
+    title,
+    cancelButton,
+    actionButtonText,
+    cancelButtonText,
+    handleCancel,
+    handleAction,
+    handleModalClose,
+    leftButtonIcon,
+    rightButtonIcon,
+    children,
+    maxWidth,
+    maxHeight,
+    footer = true,
+    footerButtonAlignment = "end"
+}: DetailedInfoModalProps) => {
+    return (
+        <div className="fixed inset-0 bg-black/85 flex items-center justify-center z-50">
+            <div className={cn("bg-white rounded-[12px] w-full flex flex-col", maxWidth, maxHeight)}>
+                <header className="flex items-center justify-between py-4 px-6 rounded-tl-[12px] rounded-tr-[12px]" style={{ background: "linear-gradient(90deg, #020926 0%, #091C71 100%)" }}>
+                    <p className="font-semibold text-xl text-white">{title}</p>
+                    <div className="rounded-[8px] bg-white min-w-6 min-h-6 cursor-pointer flex items-center justify-center">
+                        <IoCloseOutline className="min-w-4 min-h-4" onClick={handleModalClose} />
+                    </div>
+                </header>
+                <main className="my-4 mx-2 max-h-[520px] custom-scrollbar">
+                    <div className="px-2">
+                        {children}
+                    </div>
+                </main>
+                {footer && <footer className={cn("border-t border-[#DDDDDD] py-3 px-6 flex items-center", footerButtonAlignment === "end" ? "justify-end" : "justify-start")}>
+                    <div className="flex items-center justify-center gap-4">
+                        {cancelButton && (
+                            <CustomButton
+                                value={cancelButtonText || "Cancel"}
+                                handleClick={handleCancel}
+                                style={'text-sm py-2.5 px-8'}
+                                variant={"dangerSecondary"}
+                            />
+                        )}
+                        <CustomButton
+                            value={actionButtonText}
+                            handleClick={handleAction}
+                            style={'text-sm py-2.5 px-8'}
+                            variant={"primary"}
+                            rightIcon={rightButtonIcon}
+                            leftIcon={leftButtonIcon}
+                        />
+                    </div>
+                </footer>}
+            </div>
+        </div>
+    )
+}
+
+export default DetailedInfoModal
